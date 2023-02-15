29290abe01ed47db4b297410df680aa00ce53214
shahab <shahabnooshmand@gmail.com>
2023-02-15 12:40:31 +0330

    Minimum Velocity and Maximum Velocity for Actual Wind Generation

--- Hydrogen_Production_by_Wind_Turbines .py
+++ Hydrogen_Production_by_Wind_Turbines .py
@@ -6,5 +6,12 @@ import math
 from scipy.optimize import fsolve
 from scipy.interpolate import make_interp_spline
 plt.rc('figure', figsize=(15,10))
-df_wind_data = pd.read_csv('chart 1-1.csv', sep=';')
-a=df_wind_data.head()
+df_wind_data = pd.read_csv('chart 1-1.csv', )
+df_wind_data.head()
+df_wind_data.info()
+#id;year;day;datetm;min;ws_25;wd_25;tp_25;ws_50;wd_50;tp_50
+#DateTime,Actual Wind Generation  (MW),Forecast Wind Generation (MW)
+minimum_velocity = df_wind_data["Actual Wind Generation  (MW)"].min()
+maximum_velocity = df_wind_data["Actual Wind Generation  (MW)"].max()
+print('Minimum Velocity = '+str(minimum_velocity)+'MW')
+print('Maximum Velocity = '+str(maximum_velocity)+'MW')
