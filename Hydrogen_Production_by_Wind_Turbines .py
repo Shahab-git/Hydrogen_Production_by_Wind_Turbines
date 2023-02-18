0dceeb0eab54acb86c2b59f3dd3ac22e9b37104d
shahab <shahabnooshmand@gmail.com>
2023-02-18 12:01:04 +0330

    import mean_squared_error

--- Hydrogen_Production_by_Wind_Turbines .py
+++ Hydrogen_Production_by_Wind_Turbines .py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-#from sklearn2.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import math
 from scipy.optimize import fsolve
@@ -17,7 +17,7 @@ print('Minimum Velocity = '+str(minimum_velocity)+'MW')
 print('Maximum Velocity = '+str(maximum_velocity)+'MW')
 df_wind_genereation=(df_wind_data["Actual Wind Generation  (MW)"],df_wind_data['Forecast Wind Generation (MW)'])
 plt.bar(df_wind_genereation[minimum_velocity],df_wind_data['Actual Wind Generation  (MW)'], color='b')
-plt.title('minimum_velocity', fontsize=20)
-plt.ylabel('Actual Wind Generation [%]', fontsize=12)
-plt.xlabel('Minimum Velocity  (MW)', fontsize=12)
+plt.title('Minimum Velocity', fontsize=20)
+plt.ylabel('Actual Wind Generation  (MW)', fontsize=12)
+plt.xlabel('minimum_velocity', fontsize=12)
 plt.savefig('velocity_histogram.png', dpi=300, bbox_inches='tight')
